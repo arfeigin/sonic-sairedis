ba8f4e7e8f4cf1717763e7db80f201cc17e96178
afeigin <afeigin@nvidia.com>
2024-08-14 18:51:44 +0300

    Add SAI stubs VS initalizers

--- lib/sai_redis_buffer.cpp
+++ lib/sai_redis_buffer.cpp
@@ -5,7 +5,7 @@ REDIS_GENERIC_QUAD(INGRESS_PRIORITY_GROUP,ingress_priority_group);
 REDIS_GENERIC_QUAD(BUFFER_PROFILE,buffer_profile);
 REDIS_GENERIC_STATS(BUFFER_POOL,buffer_pool);
 REDIS_GENERIC_STATS(INGRESS_PRIORITY_GROUP,ingress_priority_group);
-REDIS_BULK_QUAD(INGRESS_PRIORITY_GROUP,ingress_priority_group)
+REDIS_BULK_QUAD(INGRESS_PRIORITY_GROUP,ingress_priority_group);
 
 const sai_buffer_api_t redis_buffer_api = {
 
